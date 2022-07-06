9ef8ad0a56ab3b0163ef6188c502d61443be5763
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-07-06 11:52:01 +0100

    feat: first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+//Module
+const names = require(`./4-names`);
+const sayHi = require("./5-utilis");
+const data = require("./6-alternative-flavour");
+require("./7-mind-grenade");
+
+sayHi("susan");
+sayHi(names.john);
+sayHi(names.peter);
---
+++ 8-os-module.js
@@ -0,0 +1,20 @@
+const os = require("os");
+
+// info about current user
+
+const user = os.userInfo();
+
+console.log(user);
+
+// methods returns the system uptime in seconds
+
+console.log(`the system uptime is ${os.uptime()} seconds`);
+
+const currentOs = {
+  name: os.type(),
+  release: os.release(),
+  totalmem: os.totalmem(),
+  freeMem: os.freemem(),
+};
+
+console.log(currentOs);
