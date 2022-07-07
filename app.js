45c5a4a614fecc98b9bd52d5b4733ce39245c6a5
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-07-07 21:40:40 +0100

    feat:  commit

--- app.js
+++ app.js
@@ -14,3 +14,21 @@
 //     res.end("about page");
 //   }
 // });
+
+const { readFile } = require("fs");
+
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+getText("./content/first.txt")
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
